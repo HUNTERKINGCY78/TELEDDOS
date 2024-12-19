d76b9926a65b2d4dc4f62d34680cd7a7fd86cb88
KING 👑 CY78 <hozoohunter@gmail.com>
2024-12-19 14:40:29 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,70 @@
+pip install ssl
+pip install argparse
+pip install cloudscraper
+pip install time
+pip install socket
+termux-change-repo
+pkg update
+pkg install mpv -y
+pkg install mpv -y
+pkg install jq -y
+pkg install openssh  -y
+pkg install python  -y
+pkg install python2 -y
+pkg install nano  -y
+pkg install ruby  -y
+pip install requests 
+pip install colorama
+pip install bs4
+ppip3 install Pillow
+python -m pip list
+python2 -m pip list
+python3 -m pip list
+import cv2
+PATH = cv2.__file__
+print(PATH)
+import sys
+sys.path.append('PATH')
+from tkinter import *
+from PIL import ImageTk, Image
+root.mainloop() 
+import sys
+PATH = sys.executable
+print (PATH)
+pip install pillow --upgrade
+pip freezeip install asyncio
+pip install threading
+pip install random
+pip install pystyle
+pip install Colors
+pip install Box
+pip install Write
+pip install Center
+pip install Colorate
+pip install time
+pip install ctypes
+pip install urlparse
+pip install urllib
+pip install bs4
+pip install BeautifulSoup
+pip install whois
+pip install os
+pip install telebot
+pip install traceback
+pip install PIL
+pip install string
+pip install faker
+pip install csv
+pip install asyncio
+pip install telebot 
+pip install telethon
+pip install TelegramClient
+pip install urllib
+pip install urlparse
+pip install urllib
+pip install urlopen
+pip install pystyle
+
+
+
+python botV1.py
